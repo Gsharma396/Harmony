e9e529359d2e889306e8bdc6d20370fee33d1203
RishaanDevz <113069412+RishaanDevz@users.noreply.github.com>
2023-10-19 23:53:36 -0400

    Delete pages/api/details.js

--- pages/api/details.js
+++
@@ -1,8 +0,0 @@
-// details.js
-const config = {
-  OPENAI_API_KEY: "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",
-  AKID: "AKIAYB34LVOBCYF5DDNR",
-  SAKID: "6FJuxyTjmrk+2bnmrFCXqG8MwatjIYH5/DdN46W4",
-};
-
-module.exports = config;
